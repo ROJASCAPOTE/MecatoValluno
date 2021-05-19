e13b50b069e6963f91f169bb2da8705a578163c1
jefferson-cyber <78772131+jefferson-cyber@users.noreply.github.com>
2021-05-18 20:32:56 -0500

    Create MateriaPrimaController.php
    Va en la carpeta Controller

---
+++ MateriaPrimaController.php
@@ -0,0 +1,44 @@
+<?php
+
+include_once PATH_FOLDER . 'Models/MateriaPrima.php';
+
+class MateriaPrimaController extends Controller{
+
+
+    function __construct(){
+        $this->dataUser = $this->auth();
+        $this->materia = new MateriaPrima();
+    }
+
+    function index(){
+        $this->dataUser = $this->auth(3);
+        $this->renderView('Views/admin/materia_prima/index.php', 'template');
+    }
+
+    function datos_tabla(){
+        $resul = $this->materia->traer();
+        echo json_encode($resul);
+    }
+
+    function crear(){    
+        $data = $_POST['datos'];
+        $resul = $this->materia->crear(
+            $data['descripcion'], $data['cantidad'], $data['valor']);
+        echo json_encode($resul);
+    }
+
+    function editar(){
+        $data = $_POST['datos'];
+        $resul = $this->materia->editar($data['id'],
+            $data['descripcion'], $data['cantidad'], $data['valor']);
+        echo json_encode($resul);
+    }
+
+    function eliminar(){
+        $id = $_POST['idmateria_prima'];
+        $resul = $this->materia->eliminar($id);
+        echo json_encode($resul);
+    }
+
+
+}
